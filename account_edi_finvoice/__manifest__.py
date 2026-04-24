407c9d70f859f50de59c2499b70bfd4db2bfda60
Jarmo Kortetjärvi <jarmo.kortetjarvi@futural.fi>
2026-04-24 12:26:00 +0300

    [FIX] Broken cancel-functionality

--- account_edi_finvoice/__manifest__.py
+++ account_edi_finvoice/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Import/Export invoices as Finvoice",
     "summary": "Import/Export Finvoice 3.0 invoices",
-    "version": "17.0.1.0.4",
+    "version": "17.0.1.0.5",
     "category": "Accounting",
     "website": "https://github.com/tawasta/edi",
     "author": "Futural",
